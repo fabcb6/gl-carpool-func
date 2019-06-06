e6067107f9794362b4dd1e65a7ce7c08e4e726bd
Fabian Campos <fabcb6@gmail.com>
2019-06-06 00:26:38 -0600

    Add functionality to get by UserId and restructure function

--- GetCars/index.js
+++ GetCars/index.js
@@ -3,30 +3,59 @@ const azure = require('azure-storage');
 const tableService = azure.createTableService();
 const tableName = "cars";
 
+// return a car by the Id
+const GetById = (id, context) => {
+    // return item with RowKey 'id'
+    tableService.retrieveEntity(tableName, 'Partition', id, function (error, result, response) {
+        if (!error) {
+            context.res.status(200).json(response.body);
+        }
+        else {
+            context.res.status(500).json({error : error});
+        }
+    });
+};
+
+// return the top 5 cars from a User
+const GetByUserId = (UserId, context) => {
+    var query = new azure.TableQuery()
+        .top(5)
+        .where('UserId eq ? ', UserId);
+        
+    tableService.queryEntities(tableName, query, null, function(error, result, response) {
+        if (!error) {
+            context.res.status(200).json(response.body.value);
+        }
+        else {
+            context.res.status(500).json({error : error});
+        }
+    });
+};
+
+// return the top 100 items
+const GetAll = context => {
+    var query = new azure.TableQuery().top(100);
+    tableService.queryEntities(tableName, query, null, function (error, result, response) {
+        if(!error){
+            context.res.status(200).json(response.body.value);
+        } else {
+            context.res.status(500).json({error : error});
+        }
+    });
+}
+
 module.exports = function (context, req) {
     context.log('Start ItemRead');
 
-    const id = req.query.id;
-    if (id) {
-        // return item with RowKey 'id'
-        tableService.retrieveEntity(tableName, 'Partition', id, function (error, result, response) {
-            if (!error) {
-                context.res.status(200).json(response.body);
-            }
-            else {
-                context.res.status(500).json({error : error});
-            }
-        });
+    if (req.query.id) {
+        GetById(req.query.id, context);
     }
     else {
-        // return the top 100 items
-        var query = new azure.TableQuery().top(100);
-        tableService.queryEntities(tableName, query, null, function (error, result, response) {
-            if(!error){
-                context.res.status(200).json(response.body.value);
-            } else {
-                context.res.status(500).json({error : error});
-            }
-        });
+        if (req.query.UserId) {
+            GetByUserId(req.query.UserId, context);
+        }
+        else {
+            GetAll(context);
+        }
     }
 };
